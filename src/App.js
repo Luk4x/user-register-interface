0544dc763117c1cbf1baaeb7b407eec0c71b6180
Luk4x <luk4xm4ci3l@gmail.com>
2022-06-20 20:03:05 -0300

    dinamic user

--- src/App.js
+++ src/App.js
@@ -5,9 +5,39 @@ import 'boxicons';
 
 const App = () => {
     const [users, setUsers] = useState([]);
+    const [userName, setUserName] = useState();
+    const [userAge, setUserAge] = useState();
+    const [redNameInput, setRedNameInput] = useState();
+    const [redAgeInput, setRedAgeInput] = useState();
+
+    const verifyUserName = name => {
+        if (name) return true;
+        else {
+            setRedNameInput(true);
+            return false;
+        }
+    };
+
+    const verifyUserAge = age => {
+        if (age > 0) return true;
+        else {
+            setRedAgeInput(true);
+            return false;
+        }
+    };
 
     const registerUser = () => {
-        setUsers([{ id: Math.random(), name: 'Akame', age: 16 }]);
+        if (verifyUserName(userName) && verifyUserAge(userAge)) setUsers([{ id: Math.random(), name: userName, age: userAge }]);
+    };
+
+    const setName = event => {
+        setUserName(event.target.value);
+        setRedNameInput(false);
+    };
+
+    const setAge = event => {
+        setUserAge(event.target.value);
+        setRedAgeInput(false);
     };
 
     return (
@@ -17,11 +47,23 @@ const App = () => {
                 <H1>Yo!</H1>
                 <Label>
                     <P>Name:</P>
-                    <Input placeholder="Lucas"></Input>
+                    <Input
+                        onChange={setName}
+                        placeholder="Lucas"
+                        style={{
+                            backgroundColor: redNameInput ? 'rgba(255, 110, 110, 0.25)' : 'rgba(255, 255, 255, 0.25)'
+                        }}
+                    ></Input>
                 </Label>
                 <Label>
                     <P>Age:</P>
-                    <Input placeholder="18"></Input>
+                    <Input
+                        onChange={setAge}
+                        placeholder="18"
+                        style={{
+                            backgroundColor: redAgeInput ? 'rgba(255, 110, 110, 0.25)' : 'rgba(255, 255, 255, 0.25)'
+                        }}
+                    ></Input>
                 </Label>
                 <Button onClick={registerUser}>
                     <p>Register</p>

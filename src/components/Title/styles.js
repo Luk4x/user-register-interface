2d71214581655e9b37cab398351b05b7fc1a627f
Luk4x <luk4xm4ci3l@gmail.com>
2022-07-10 15:39:43 -0300

    Title update

--- src/components/Title/styles.js
+++ src/components/Title/styles.js
@@ -6,10 +6,10 @@ export const H1 = styled.h1`
     line-height: 40px;
     color: whitesmoke;
     margin-bottom: 15px;
-    background: linear-gradient(45deg, #6317a9, #c89bf1);
+    background: linear-gradient(45deg, #330055, #dd00ff 60%);
     -webkit-background-clip: text;
     -moz-background-clip: text;
     -webkit-text-fill-color: transparent;
     -moz-text-fill-color: transparent;
-    text-shadow: 1px 1px 4px #6317a9bb;
+    text-shadow: 1px 1px 4px #330055bb;
 `;
